09ea1116e543216662202c7d333cb0b5cc7c47fc
da-yoshi-k <hey234234s@gmail.com>
2023-04-06 20:51:10 +0900

    fix: メソッドの整理とビューの一部修正

--- app/models/open_ai_api.rb
+++ app/models/open_ai_api.rb
@@ -3,11 +3,7 @@ class OpenAiApi
 
   def self.generate_suggestions(keyword = '')
     client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
-    prompt_content = 'アイスブレークに合っている質問を最大5個まで提供してください。「・」、「- 」、連番は回答内に不要です。日本語で回答してください。改行区切りで質問文を5個並べてください。生成できない場合はエラーである旨を返してください。'
-    keyword_content = "キーワード「#{keyword}」に関連する。"
-    keyword_description = '質問文中にキーワードを含めるのは5個中の3個までとして、そのほか2つは遠くないけどキーワードが直接入らない質問を生成してください。'
-    prompt_content = keyword_content + prompt_content if keyword.present?
-    prompt_content += keyword_description if keyword.present?
+    prompt_content = build_prompt_for_suggestion(keyword)
     response = client.chat(
       parameters: {
         model: 'gpt-3.5-turbo',
@@ -15,7 +11,20 @@ class OpenAiApi
       }
     )
     suggestions = response.dig('choices', 0, 'message', 'content').split("\n")
-    # 配列要素先頭の「・」「- 」「1. 」といった記号を削除
+    clean_suggestions(suggestions)
+  end
+
+  def self.build_prompt_for_suggestion(keyword)
+    prompt_content = 'アイスブレークにピッタリな面白い質問を最大5個まで生成してください。「・」、「- 」、連番は回答内に不要です。日本語で回答してください。質問文を改行区切りで5つ出力してください。生成できない場合はエラーである旨を返してください。'
+    keyword_content = "キーワード「#{keyword}」に関連する、"
+    keyword_description = 'キーワードを含めた適切な質問を最大3つ、他の2つはキーワードを直接含まず違う角度から関連性を考えた質問を生成してください。'
+    prompt_content = keyword_content + prompt_content if keyword.present?
+    prompt_content += keyword_description if keyword.present?
+    prompt_content
+  end
+
+  # 配列要素先頭の「・」「- 」「1. 」といった記号を削除
+  def self.clean_suggestions(suggestions)
     suggestions.map! { |suggestion| suggestion.gsub(/・|- |\d+\. /, '') }
   end
 end

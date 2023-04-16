9c237d3c7f34c22705003111245a96546c0922cb
da-yoshi-k <hey234234s@gmail.com>
2023-04-16 17:57:38 +0900

    add: meta-tagsの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'AIトークデッキ',
+      separator: '|',
+      description: 'AIがキーワードから会話の話題を生成するサービスです。',
+      keywords: 'AI,話題,キーワード',
+      canonical: 'https://ai-talk-deck.fly.dev/',
+      noindex: !Rails.env.production?,
+      og: {
+        site_name: 'AIトークデッキ',
+        description: 'AIがキーワードから会話の話題を生成するサービスです。',
+        type: 'website',
+        url: 'https://ai-talk-deck.fly.dev/',
+        image: image_url('/ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image'
+      }
+    }
+  end
 end

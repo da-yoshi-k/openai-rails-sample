04d1ddfbce366fc4f4c2b21e67c1711d4bbe4151
da-yoshi-k <hey234234s@gmail.com>
2023-04-16 19:49:59 +0900

    fix: メタデータの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,18 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'AIトークデッキ',
-      separator: '|',
+      title: 'AIトークデッキ',
       description: 'AIがキーワードから会話の話題を生成するサービスです。',
       keywords: 'AI,話題,キーワード',
       canonical: 'https://ai-talk-deck.fly.dev/',
       noindex: !Rails.env.production?,
       og: {
         site_name: 'AIトークデッキ',
+        title: 'AIトークデッキ',
         description: 'AIがキーワードから会話の話題を生成するサービスです。',
         type: 'website',
         url: 'https://ai-talk-deck.fly.dev/',
-        image: image_url('/ogp.png'),
+        image: 'https://ai-talk-deck.fly.dev/ogp.png',
         locale: 'ja_JP'
       },
       twitter: {
